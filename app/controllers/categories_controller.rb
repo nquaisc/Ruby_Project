f5636877e05e2f93efb061a4027917e3b355701c
Nathan Quaiscer <nathanquaiscer@Nathans-MacBook-Pro.local>
2017-11-22 12:45:35 -0600

    Added a search bar to the index view of the category model

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,15 @@
 class CategoriesController < ApplicationController
+  
+  
+  
   def index
+    
+    @product = Product.all
+    
+    if params[:keyword]
+      @categories = Category.search(params[:keyword]).order("name ASC")
+    end
+    
   end
+
 end
